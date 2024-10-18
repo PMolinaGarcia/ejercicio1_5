f7d3d878c705f22313294929af6f6b9ea8b7834e
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2024-10-18 19:51:43 +0200

    Actualización Municipio y creación Provincia

---
+++ dominio/Provincia.java
@@ -0,0 +1,23 @@
+package dominio;
+import java.util.ArrayList;
+import java.util.*;
+import java.io.Serializable;
+//Creamos la clase tras importar todo lo necesario y decir el paquete de la clase
+public class Provincia implements Serializable{
+    //Creamos un ArrayList con los municipios que se añadan a la provincia
+    private ArrayList<Municipio> municipiosProvincia = new ArrayList<>();
+    //Método para añadir municipios a la provincia
+    public void añadir (Municipio municipio){
+        municipiosProvincia.add(municipio);
+    }
+
+    public ArrayList getMunicipiosProvincia() {
+        return municipiosProvincia;
+    }
+    public int contarHabitantes(){
+        int suma = 0;
+        for (Municipio municipio : municipiosProvincia) {
+            suma += Integer.parseInt(municipio.contarHabitantes());
+        }
+    }
+}

6365d2ad96ca9b14734b88ad754a59dcba06bf88
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2024-10-18 19:38:44 +0200

    Añadimos Municipio

---
+++ dominio/Municipio.java
@@ -0,0 +1,26 @@
+package dominio;
+import java.util.ArrayList;
+import java.util.*;
+import java.io.Serializable;
+//Creamos la clase tras importar todo lo necesario y decir el paquete de la clase
+public class Municipio implements Serializable{
+    //Creamos un ArrayList con las localidades que se añadan al municipio
+    private ArrayList<Localidad> localidadesMunicipio = new ArrayList<>();
+    //Método para añadir localidades al municipio
+    public void añadir (Localidad localidad){
+        localidadesMunicipio.add(localidad);
+    }
+
+    public ArrayList getLocalidadesMunicipio() {
+        return localidadesMunicipio;
+    }
+    public int contarHabitantes(){
+        int suma = 0;
+        for (Localidad localidad : localidadesMunicipio) {
+            suma = numeroHabitantes;
+        }
+    }
+}
+
+
+}
